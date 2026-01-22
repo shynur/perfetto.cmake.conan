b7ae18da7e36011b59d465b336884f67c08bf90d
shynur <shynur@outlook.com>
2026-01-22 17:30:19 +0800

    ;

--- contrib/install-tracebox.bash
+++ contrib/install-tracebox.bash
@@ -4,8 +4,4 @@ cd /tmp
 rm -f tracebox
 curl -LO https://get.perfetto.dev/tracebox
 chmod +x tracebox
-if which sudo &>/dev/null && [ -x `which sudo` ]; then
-    sudo mv tracebox /usr/local/bin/
-else
-    mv tracebox /usr/local/bin/
-fi
+mv tracebox /usr/local/bin/
