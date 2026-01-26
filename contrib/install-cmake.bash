81a84ff89344e92b5eb631632e154813dc133736
shynur <shynur@outlook.com>
2026-01-26 15:56:53 +0800

    Update install-cmake.bash

--- contrib/install-cmake.bash
+++ contrib/install-cmake.bash
@@ -15,7 +15,7 @@ fi
 
 cd /tmp
 if ! [ -f cmake-3.31.10-linux-$HOSTTYPE.sh ]; then
-    curl -LO https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-linux-$HOSTTYPE.sh
+    curl --remove-on-error -LO https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-linux-$HOSTTYPE.sh
 fi
 bash cmake-3.31.10-linux-$HOSTTYPE.sh --skip-license --prefix=/usr/local/ --exclude-subdir
 
