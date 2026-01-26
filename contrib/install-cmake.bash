1af7ebd500fa5b9a4956eafb926d9eb10656bdd3
shynur <shynur@outlook.com>
2026-01-26 15:51:25 +0800

    Update install-cmake.bash

--- contrib/install-cmake.bash
+++ contrib/install-cmake.bash
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+NEED_INSTALL_CMAKE=
+if ! [ which cmake ]; then
+    NEED_INSTALL_CMAKE=1
+elif ! cmake --version >/dev/null; then
+    NEED_INSTALL_CMAKE=1
+elif [ v3.23 = v`{ echo 3.23; cmake --version | egrep '^cmake version [[:digit:]]+[.][[:digit:]]+' | awk '{printf $3}'; } | sort -V | head -1` ]; then
+    NEED_INSTALL_CMAKE=1
+fi
+
+if ! [ "$NEED_INSTALL_CMAKE" ]; then
+    exit
+fi
+
 cd /tmp
 if ! [ -f cmake-3.31.10-linux-$HOSTTYPE.sh ]; then
     curl -LO https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-linux-$HOSTTYPE.sh
 fi
 bash cmake-3.31.10-linux-$HOSTTYPE.sh --skip-license --prefix=/usr/local/ --exclude-subdir
+
 /usr/local/bin/cmake --version
